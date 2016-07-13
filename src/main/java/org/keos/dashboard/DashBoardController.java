507b0a4d2bb6928b0948e034ffdba9acf3a70f0a
shishira <shishira@live.com>
2016-07-13 16:58:18 +0530

    google anlytics javascript function added in index page

--- src/main/java/org/keos/dashboard/DashBoardController.java
+++ src/main/java/org/keos/dashboard/DashBoardController.java
@@ -1,17 +1,51 @@
 package org.keos.dashboard;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/dashBoardController")
 public class DashBoardController {
-	
-	@RequestMapping(value="/getDashBoardName", method=RequestMethod.GET)
-	public String getDashBoardName(){
+
+	@RequestMapping(value = "/getDashBoardName", method = RequestMethod.GET)
+	public ResponseEntity<?> getDashBoardName() {
 		System.out.println("getDashBoardName #####################");
-		return "ADMIN";
+		// return "ADMIN";
+		return new ResponseEntity<String>("success", HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/getDonutChartData", method = RequestMethod.GET, produces="application/json")
+	public @ResponseBody Map<String, Integer> getDonutChartData() {
+
+		System.out.println(" ##################### getDonutChartData ##################### ");
+
+		Map<String, Integer> m = new HashMap<String, Integer>();
+		m.put("Download Sales", 20);
+		m.put("Download Marketing", 40);
+		m.put("Download Inbound", 20);
+		m.put("Download Outbound", 10);
+		m.put("Download Timer", 10);
+
+		
+		/*String s = "";
+		JSONObject jsonObj = new JSONObject(s);*/
+		
+		/*
+		 * List<JSONObject> entities = new ArrayList<JSONObject>();
+		 * 
+		 * JSONObject entity1 = new JSONObject(); entity1.put("label",
+		 * "Download Sales"); entity1.put("value",12); entities.add(entity1);
+		 */
+
+		return m;
 	}
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,10 +1,43 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <html>
-<body>
-<h2>Home</h2>
+<head>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-<a href="<c:url value="/dashboard/pages/dashboard.html"/>">dash board</a>
-<a href="userController/getUserDetails">user details</a>
-<a href="<c:url value="/dashboard/pages/login.html"/>">Login dash board</a>
+  ga('create', 'UA-80716845-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</head>
+<body>
+	<h2>Home</h2>
+
+	<a href="<c:url value="/dashboard/pages/dashboard.html"/>">dash
+		board</a>
+	<a href="userController/getUserDetails">user details</a>
+	<a href="<c:url value="/dashboard/pages/login.html"/>">Login dash
+		board</a>
 </body>
+
+<!-- <div class="fb-share-button"
+	data-href="https://developers.facebook.com/docs/plugins/"
+	data-layout="button_count" data-mobile-iframe="true"></div>
+
+
+<div id="fb-root"></div> -->
+
+<!-- <script>
+	(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id))
+			return;
+		js = d.createElement(s);
+		js.id = id;
+		js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.6";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+</script> -->
 </html>
